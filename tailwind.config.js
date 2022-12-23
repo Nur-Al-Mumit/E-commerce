1c5a4909333c04347ea0c38399c51010390583f7
nuralmumit <nuralmumit@nuralmumit.nuralmumit>
2022-12-23 12:17:06 +0600

    added about section's animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          from: { transform: "translateY(100%)", opacity: "0" },
+          to: { transform: "translateY(0%)", opacity: "1" },
+        },
+        getscl: {
+          "0%": {
+            transform: "scale(1)",
+          },
+          "50%": {
+            transform: "scale(1.1)",
+          },
+          "100%": {
+            transform: "scale(1)",
+          },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 2s ease-in-out",
+        getscl: "getscl 1s ease-in-out 200ms",
+      },
+    },
   },
   plugins: [],
-}
+};
